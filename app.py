4dbd4d2945015a51d327b3d894033aa50b949d97
harish379-k <hpplayzg@gmail.com>
2026-05-05 11:47:03 +0530

    Fix Groq model name

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ CORS(app, origins="*")
 sessions = {}
 
 groq_client = Groq(api_key=os.environ.get("GROQ_API_KEY", ""))
-GROQ_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
+GROQ_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
 
 
 # -----------------------------
